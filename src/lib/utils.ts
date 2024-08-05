4e26eb7223a4876b59d21dc4dc15425e14836004
Nick <nconkl1@outlook.com>
2024-08-05 15:19:15 -0400

    :bug: fixed an issue in the logic of determining the next post

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,7 +12,14 @@ export function getPreviousPost(currentSlug: string) {
   const currentPostIndex = posts.findIndex((p) => p.slug === currentSlug);
   if (currentPostIndex > 0) {
     return posts[currentPostIndex - 1];
-  } else {
+  }
+  return null; // Return null if there is no previous post
+}
+
+export function getNextPost(currentSlug: string) {
+  const currentPostIndex = posts.findIndex((p) => p.slug === currentSlug);
+  if (currentPostIndex < posts.length - 1) {
     return posts[currentPostIndex + 1];
   }
+  return null; // Return null if there is no next post
 }
